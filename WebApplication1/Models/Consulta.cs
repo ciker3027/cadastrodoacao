d88f55a2d572cc1fb787f5e969246ea5a6c6f40c
Erick <erick.kkk@hotmail.com>
2022-10-08 16:20:22 -0300

    commit

---
+++ WebApplication1/Models/Consulta.cs
@@ -0,0 +1,10 @@
+namespace Cadastro_de_doacao.Models
+{
+    public class Consulta
+    {
+         public string? AnimalModel { get; set; }
+         public string? Veterinario { get; set; }
+         public string? Consulta { get; set; }
+
+    }
+}
