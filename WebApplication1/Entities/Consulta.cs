97469fe3a4feb619f589ca2c3a1a4c70fbbe3fe4
Erick <erick.kkk@hotmail.com>
2022-10-08 16:20:07 -0300

    criaçao

---
+++ WebApplication1/Entities/Consulta.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+    public class Consulta
+    {
+         public string AnimalModel { get; set; }
+         public string Veterinario { get; set; }
+         public string Consulta { get; set; }
+
+    }
